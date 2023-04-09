45e153b3f69308c82d3b805aac59bdb0056b3741
Erix Gutierrez <erix.gutierrez@gmail.com>
2023-04-09 01:15:31 -0400

    testing cors

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(credentials);
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
